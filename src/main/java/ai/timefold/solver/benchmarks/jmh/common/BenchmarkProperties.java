a4015f99b48d9d68a2233996126ed739bb224a4c
Lukáš Petrovický <lukas@petrovicky.net>
2024-03-14 13:53:13 +0100

    Clean up packages

--- src/main/java/ai/timefold/solver/benchmarks/jmh/common/BenchmarkProperties.java
+++
@@ -1,6 +0,0 @@
-package ai.timefold.solver.benchmarks.jmh.common;
-
-public record BenchmarkProperties(int forkCount, int warmupIterations, int measurementIterations,
-        double relativeScoreErrorThreshold) {
-
-}
--- src/main/java/ai/timefold/solver/benchmarks/jmh/common/ThrowingFunction.java
+++
@@ -1,9 +0,0 @@
-package ai.timefold.solver.benchmarks.jmh.common;
-
-import java.io.IOException;
-
-public interface ThrowingFunction<A, B> {
-
-    B apply(A a) throws IOException;
-
-}
