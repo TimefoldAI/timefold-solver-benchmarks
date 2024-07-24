bc34d550ef7170a1d5ac9f20e063becf7a4de6f6
Lukáš Petrovický <lukas@petrovicky.net>
2024-07-24 08:44:57 +0200

    chore: fix examination deserialization

--- src/main/java/ai/timefold/solver/benchmarks/examples/examination/persistence/ExaminationJacksonModule.java
+++ src/main/java/ai/timefold/solver/benchmarks/examples/examination/persistence/ExaminationJacksonModule.java
@@ -7,6 +7,7 @@ import com.fasterxml.jackson.databind.module.SimpleModule;
 public final class ExaminationJacksonModule extends SimpleModule {
 
     public ExaminationJacksonModule() {
+        super("ExaminationJacksonModule");
         addDeserializer(ConstraintWeightOverrides.class, new ExaminationConstraintWeightOverrideDeserializer());
     }
 
