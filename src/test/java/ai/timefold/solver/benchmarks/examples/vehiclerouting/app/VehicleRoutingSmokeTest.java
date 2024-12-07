1658b26c07e639ee10780690160654ee22da473f
Lukáš Petrovický <lukas@petrovicky.net>
2024-12-07 15:52:20 +0100

    chore: fix smoke test threshold

--- src/test/java/ai/timefold/solver/benchmarks/examples/vehiclerouting/app/VehicleRoutingSmokeTest.java
+++ src/test/java/ai/timefold/solver/benchmarks/examples/vehiclerouting/app/VehicleRoutingSmokeTest.java
@@ -21,10 +21,10 @@ class VehicleRoutingSmokeTest extends SolverSmokeTest<VehicleRoutingSolution, Ha
     protected Stream<TestData<HardSoftLongScore>> testData() {
         return Stream.of(
                 TestData.of(ConstraintStreamImplType.BAVET, CVRP_32_CUSTOMERS,
-                        HardSoftLongScore.ofSoft(-788369),
-                        HardSoftLongScore.ofSoft(-788369)),
+                        HardSoftLongScore.ofSoft(-744),
+                        HardSoftLongScore.ofSoft(-744)),
                 TestData.of(ConstraintStreamImplType.BAVET, CVRPTW_100_CUSTOMERS_A,
-                        HardSoftLongScore.ofSoft(-1965310),
-                        HardSoftLongScore.ofSoft(-2112382)));
+                        HardSoftLongScore.ofSoft(-1661),
+                        HardSoftLongScore.ofSoft(-1661)));
     }
 }
