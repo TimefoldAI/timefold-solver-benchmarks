301b8f4ca25c84c0f88abe197434a1e76a103600
Fred <zepfred@gmail.com>
2025-02-05 11:50:59 -0300

    chore: update script

--- run-cvrplib-experiment.sh
+++ run-cvrplib-experiment.sh
@@ -1,42 +1,43 @@
 #!/bin/bash
-nohup java -Xmx4g -XX:+UseParallelGC -cp /tmp/benchmarks-restart.jar ai.timefold.solver.benchmarks.competitive.cvrplib.Main COMMUNITY_EDITION 400 > /tmp/nohup1.out 2>&1
-nohup java -Xmx4g -XX:+UseParallelGC -cp /tmp/benchmarks-restart.jar ai.timefold.solver.benchmarks.competitive.cvrplib.Main COMMUNITY_EDITION 100 > /tmp/nohup2.out 2>&1
-nohup java -Xmx4g -XX:+UseParallelGC -cp /tmp/benchmarks-main.jar ai.timefold.solver.benchmarks.competitive.cvrplib.Main ENTERPRISE_EDITION 400 > /tmp/nohup3.out 2>&1
-nohup java -Xmx4g -XX:+UseParallelGC -cp /tmp/benchmarks-main.jar ai.timefold.solver.benchmarks.competitive.cvrplib.Main ENTERPRISE_EDITION 400 > /tmp/nohup4.out 2>&1
-nohup java -Xmx4g -XX:+UseParallelGC -cp /tmp/benchmarks-main.jar ai.timefold.solver.benchmarks.competitive.cvrplib.Main ENTERPRISE_EDITION 1000 > /tmp/nohup5.out 2>&1
-nohup java -Xmx4g -XX:+UseParallelGC -cp /tmp/benchmarks-main.jar ai.timefold.solver.benchmarks.competitive.cvrplib.Main COMMUNITY_EDITION 100 > /tmp/nohup6.out 2>&1
-nohup java -Xmx4g -XX:+UseParallelGC -cp /tmp/benchmarks-restart.jar ai.timefold.solver.benchmarks.competitive.cvrplib.Main ENTERPRISE_EDITION 1000 > /tmp/nohup7.out 2>&1
-nohup java -Xmx4g -XX:+UseParallelGC -cp /tmp/benchmarks-restart.jar ai.timefold.solver.benchmarks.competitive.cvrplib.Main COMMUNITY_EDITION 1000 > /tmp/nohup8.out 2>&1
-nohup java -Xmx4g -XX:+UseParallelGC -cp /tmp/benchmarks-main.jar ai.timefold.solver.benchmarks.competitive.cvrplib.Main COMMUNITY_EDITION 1000 > /tmp/nohup9.out 2>&1
-nohup java -Xmx4g -XX:+UseParallelGC -cp /tmp/benchmarks-main.jar ai.timefold.solver.benchmarks.competitive.cvrplib.Main COMMUNITY_EDITION 100 > /tmp/nohup10.out 2>&1
+mkdir -p experiment
+nohup java -Xmx4g -XX:+UseParallelGC -cp ./experiment/benchmarks-restart.jar ai.timefold.solver.benchmarks.competitive.cvrplib.Main COMMUNITY_EDITION 400 > ./experiment/nohup1.out 2>&1
+nohup java -Xmx4g -XX:+UseParallelGC -cp ./experiment/benchmarks-restart.jar ai.timefold.solver.benchmarks.competitive.cvrplib.Main COMMUNITY_EDITION 100 > ./experiment/nohup2.out 2>&1
+nohup java -Xmx4g -XX:+UseParallelGC -cp ./experiment/benchmarks-main.jar ai.timefold.solver.benchmarks.competitive.cvrplib.Main ENTERPRISE_EDITION 400 > ./experiment/nohup3.out 2>&1
+nohup java -Xmx4g -XX:+UseParallelGC -cp ./experiment/benchmarks-main.jar ai.timefold.solver.benchmarks.competitive.cvrplib.Main ENTERPRISE_EDITION 400 > ./experiment/nohup4.out 2>&1
+nohup java -Xmx4g -XX:+UseParallelGC -cp ./experiment/benchmarks-main.jar ai.timefold.solver.benchmarks.competitive.cvrplib.Main ENTERPRISE_EDITION 1000 > ./experiment/nohup5.out 2>&1
+nohup java -Xmx4g -XX:+UseParallelGC -cp ./experiment/benchmarks-main.jar ai.timefold.solver.benchmarks.competitive.cvrplib.Main COMMUNITY_EDITION 100 > ./experiment/nohup6.out 2>&1
+nohup java -Xmx4g -XX:+UseParallelGC -cp ./experiment/benchmarks-restart.jar ai.timefold.solver.benchmarks.competitive.cvrplib.Main ENTERPRISE_EDITION 1000 > ./experiment/nohup7.out 2>&1
+nohup java -Xmx4g -XX:+UseParallelGC -cp ./experiment/benchmarks-restart.jar ai.timefold.solver.benchmarks.competitive.cvrplib.Main COMMUNITY_EDITION 1000 > ./experiment/nohup8.out 2>&1
+nohup java -Xmx4g -XX:+UseParallelGC -cp ./experiment/benchmarks-main.jar ai.timefold.solver.benchmarks.competitive.cvrplib.Main COMMUNITY_EDITION 1000 > ./experiment/nohup9.out 2>&1
+nohup java -Xmx4g -XX:+UseParallelGC -cp ./experiment/benchmarks-main.jar ai.timefold.solver.benchmarks.competitive.cvrplib.Main COMMUNITY_EDITION 100 > ./experiment/nohup10.out 2>&1
 
-nohup java -Xmx4g -XX:+UseParallelGC -cp /tmp/benchmarks-restart.jar ai.timefold.solver.benchmarks.competitive.cvrplib.Main ENTERPRISE_EDITION 100 > /tmp/nohup11.out 2>&1
-nohup java -Xmx4g -XX:+UseParallelGC -cp /tmp/benchmarks-main.jar ai.timefold.solver.benchmarks.competitive.cvrplib.Main COMMUNITY_EDITION 1000 > /tmp/nohup12.out 2>&1
-nohup java -Xmx4g -XX:+UseParallelGC -cp /tmp/benchmarks-main.jar ai.timefold.solver.benchmarks.competitive.cvrplib.Main ENTERPRISE_EDITION 1000 > /tmp/nohup13.out 2>&1
-nohup java -Xmx4g -XX:+UseParallelGC -cp /tmp/benchmarks-restart.jar ai.timefold.solver.benchmarks.competitive.cvrplib.Main ENTERPRISE_EDITION 400 > /tmp/nohup14.out 2>&1
-nohup java -Xmx4g -XX:+UseParallelGC -cp /tmp/benchmarks-main.jar ai.timefold.solver.benchmarks.competitive.cvrplib.Main ENTERPRISE_EDITION 100 > /tmp/nohup15.out 2>&1
-nohup java -Xmx4g -XX:+UseParallelGC -cp /tmp/benchmarks-restart.jar ai.timefold.solver.benchmarks.competitive.cvrplib.Main ENTERPRISE_EDITION 1000 > /tmp/nohup16.out 2>&1
-nohup java -Xmx4g -XX:+UseParallelGC -cp /tmp/benchmarks-restart.jar ai.timefold.solver.benchmarks.competitive.cvrplib.Main COMMUNITY_EDITION 100 > /tmp/nohup17.out 2>&1
-nohup java -Xmx4g -XX:+UseParallelGC -cp /tmp/benchmarks-restart.jar ai.timefold.solver.benchmarks.competitive.cvrplib.Main ENTERPRISE_EDITION 1000 > /tmp/nohup18.out 2>&1
-nohup java -Xmx4g -XX:+UseParallelGC -cp /tmp/benchmarks-restart.jar ai.timefold.solver.benchmarks.competitive.cvrplib.Main ENTERPRISE_EDITION 100 > /tmp/nohup19.out 2>&1
-nohup java -Xmx4g -XX:+UseParallelGC -cp /tmp/benchmarks-restart.jar ai.timefold.solver.benchmarks.competitive.cvrplib.Main COMMUNITY_EDITION 1000 > /tmp/nohup20.out 2>&1
+nohup java -Xmx4g -XX:+UseParallelGC -cp ./experiment/benchmarks-restart.jar ai.timefold.solver.benchmarks.competitive.cvrplib.Main ENTERPRISE_EDITION 100 > ./experiment/nohup11.out 2>&1
+nohup java -Xmx4g -XX:+UseParallelGC -cp ./experiment/benchmarks-main.jar ai.timefold.solver.benchmarks.competitive.cvrplib.Main COMMUNITY_EDITION 1000 > ./experiment/nohup12.out 2>&1
+nohup java -Xmx4g -XX:+UseParallelGC -cp ./experiment/benchmarks-main.jar ai.timefold.solver.benchmarks.competitive.cvrplib.Main ENTERPRISE_EDITION 1000 > ./experiment/nohup13.out 2>&1
+nohup java -Xmx4g -XX:+UseParallelGC -cp ./experiment/benchmarks-restart.jar ai.timefold.solver.benchmarks.competitive.cvrplib.Main ENTERPRISE_EDITION 400 > ./experiment/nohup14.out 2>&1
+nohup java -Xmx4g -XX:+UseParallelGC -cp ./experiment/benchmarks-main.jar ai.timefold.solver.benchmarks.competitive.cvrplib.Main ENTERPRISE_EDITION 100 > ./experiment/nohup15.out 2>&1
+nohup java -Xmx4g -XX:+UseParallelGC -cp ./experiment/benchmarks-restart.jar ai.timefold.solver.benchmarks.competitive.cvrplib.Main ENTERPRISE_EDITION 1000 > ./experiment/nohup16.out 2>&1
+nohup java -Xmx4g -XX:+UseParallelGC -cp ./experiment/benchmarks-restart.jar ai.timefold.solver.benchmarks.competitive.cvrplib.Main COMMUNITY_EDITION 100 > ./experiment/nohup17.out 2>&1
+nohup java -Xmx4g -XX:+UseParallelGC -cp ./experiment/benchmarks-restart.jar ai.timefold.solver.benchmarks.competitive.cvrplib.Main ENTERPRISE_EDITION 1000 > ./experiment/nohup18.out 2>&1
+nohup java -Xmx4g -XX:+UseParallelGC -cp ./experiment/benchmarks-restart.jar ai.timefold.solver.benchmarks.competitive.cvrplib.Main ENTERPRISE_EDITION 100 > ./experiment/nohup19.out 2>&1
+nohup java -Xmx4g -XX:+UseParallelGC -cp ./experiment/benchmarks-restart.jar ai.timefold.solver.benchmarks.competitive.cvrplib.Main COMMUNITY_EDITION 1000 > ./experiment/nohup20.out 2>&1
 
-nohup java -Xmx4g -XX:+UseParallelGC -cp /tmp/benchmarks-main.jar ai.timefold.solver.benchmarks.competitive.cvrplib.Main COMMUNITY_EDITION 1000 > /tmp/nohup21.out 2>&1
-nohup java -Xmx4g -XX:+UseParallelGC -cp /tmp/benchmarks-restart.jar ai.timefold.solver.benchmarks.competitive.cvrplib.Main ENTERPRISE_EDITION 100 > /tmp/nohup22.out 2>&1
-nohup java -Xmx4g -XX:+UseParallelGC -cp /tmp/benchmarks-restart.jar ai.timefold.solver.benchmarks.competitive.cvrplib.Main ENTERPRISE_EDITION 400 > /tmp/nohup23.out 2>&1
-nohup java -Xmx4g -XX:+UseParallelGC -cp /tmp/benchmarks-restart.jar ai.timefold.solver.benchmarks.competitive.cvrplib.Main COMMUNITY_EDITION 400 > /tmp/nohup24.out 2>&1
-nohup java -Xmx4g -XX:+UseParallelGC -cp /tmp/benchmarks-restart.jar ai.timefold.solver.benchmarks.competitive.cvrplib.Main ENTERPRISE_EDITION 400 > /tmp/nohup25.out 2>&1
-nohup java -Xmx4g -XX:+UseParallelGC -cp /tmp/benchmarks-restart.jar ai.timefold.solver.benchmarks.competitive.cvrplib.Main COMMUNITY_EDITION 400 > /tmp/nohup26.out 2>&1
-nohup java -Xmx4g -XX:+UseParallelGC -cp /tmp/benchmarks-main.jar ai.timefold.solver.benchmarks.competitive.cvrplib.Main COMMUNITY_EDITION 400 > /tmp/nohup27.out 2>&1
-nohup java -Xmx4g -XX:+UseParallelGC -cp /tmp/benchmarks-main.jar ai.timefold.solver.benchmarks.competitive.cvrplib.Main ENTERPRISE_EDITION 100 > /tmp/nohup28.out 2>&1
-nohup java -Xmx4g -XX:+UseParallelGC -cp /tmp/benchmarks-restart.jar ai.timefold.solver.benchmarks.competitive.cvrplib.Main COMMUNITY_EDITION 100 > /tmp/nohup29.out 2>&1
-nohup java -Xmx4g -XX:+UseParallelGC -cp /tmp/benchmarks-main.jar ai.timefold.solver.benchmarks.competitive.cvrplib.Main ENTERPRISE_EDITION 1000 > /tmp/nohup30.out 2>&1
+nohup java -Xmx4g -XX:+UseParallelGC -cp ./experiment/benchmarks-main.jar ai.timefold.solver.benchmarks.competitive.cvrplib.Main COMMUNITY_EDITION 1000 > ./experiment/nohup21.out 2>&1
+nohup java -Xmx4g -XX:+UseParallelGC -cp ./experiment/benchmarks-restart.jar ai.timefold.solver.benchmarks.competitive.cvrplib.Main ENTERPRISE_EDITION 100 > ./experiment/nohup22.out 2>&1
+nohup java -Xmx4g -XX:+UseParallelGC -cp ./experiment/benchmarks-restart.jar ai.timefold.solver.benchmarks.competitive.cvrplib.Main ENTERPRISE_EDITION 400 > ./experiment/nohup23.out 2>&1
+nohup java -Xmx4g -XX:+UseParallelGC -cp ./experiment/benchmarks-restart.jar ai.timefold.solver.benchmarks.competitive.cvrplib.Main COMMUNITY_EDITION 400 > ./experiment/nohup24.out 2>&1
+nohup java -Xmx4g -XX:+UseParallelGC -cp ./experiment/benchmarks-restart.jar ai.timefold.solver.benchmarks.competitive.cvrplib.Main ENTERPRISE_EDITION 400 > ./experiment/nohup25.out 2>&1
+nohup java -Xmx4g -XX:+UseParallelGC -cp ./experiment/benchmarks-restart.jar ai.timefold.solver.benchmarks.competitive.cvrplib.Main COMMUNITY_EDITION 400 > ./experiment/nohup26.out 2>&1
+nohup java -Xmx4g -XX:+UseParallelGC -cp ./experiment/benchmarks-main.jar ai.timefold.solver.benchmarks.competitive.cvrplib.Main COMMUNITY_EDITION 400 > ./experiment/nohup27.out 2>&1
+nohup java -Xmx4g -XX:+UseParallelGC -cp ./experiment/benchmarks-main.jar ai.timefold.solver.benchmarks.competitive.cvrplib.Main ENTERPRISE_EDITION 100 > ./experiment/nohup28.out 2>&1
+nohup java -Xmx4g -XX:+UseParallelGC -cp ./experiment/benchmarks-restart.jar ai.timefold.solver.benchmarks.competitive.cvrplib.Main COMMUNITY_EDITION 100 > ./experiment/nohup29.out 2>&1
+nohup java -Xmx4g -XX:+UseParallelGC -cp ./experiment/benchmarks-main.jar ai.timefold.solver.benchmarks.competitive.cvrplib.Main ENTERPRISE_EDITION 1000 > ./experiment/nohup30.out 2>&1
 
-nohup java -Xmx4g -XX:+UseParallelGC -cp /tmp/benchmarks-main.jar ai.timefold.solver.benchmarks.competitive.cvrplib.Main COMMUNITY_EDITION 100 > /tmp/nohup31.out 2>&1
-nohup java -Xmx4g -XX:+UseParallelGC -cp /tmp/benchmarks-main.jar ai.timefold.solver.benchmarks.competitive.cvrplib.Main ENTERPRISE_EDITION 100 > /tmp/nohup32.out 2>&1
-nohup java -Xmx4g -XX:+UseParallelGC -cp /tmp/benchmarks-main.jar ai.timefold.solver.benchmarks.competitive.cvrplib.Main COMMUNITY_EDITION 400 > /tmp/nohup33.out 2>&1
-nohup java -Xmx4g -XX:+UseParallelGC -cp /tmp/benchmarks-main.jar ai.timefold.solver.benchmarks.competitive.cvrplib.Main COMMUNITY_EDITION 400 > /tmp/nohup34.out 2>&1
-nohup java -Xmx4g -XX:+UseParallelGC -cp /tmp/benchmarks-main.jar ai.timefold.solver.benchmarks.competitive.cvrplib.Main ENTERPRISE_EDITION 400 > /tmp/nohup35.out 2>&1
-nohup java -Xmx4g -XX:+UseParallelGC -cp /tmp/benchmarks-restart.jar ai.timefold.solver.benchmarks.competitive.cvrplib.Main COMMUNITY_EDITION 1000 > /tmp/nohup36.out 2>&1
+nohup java -Xmx4g -XX:+UseParallelGC -cp ./experiment/benchmarks-main.jar ai.timefold.solver.benchmarks.competitive.cvrplib.Main COMMUNITY_EDITION 100 > ./experiment/nohup31.out 2>&1
+nohup java -Xmx4g -XX:+UseParallelGC -cp ./experiment/benchmarks-main.jar ai.timefold.solver.benchmarks.competitive.cvrplib.Main ENTERPRISE_EDITION 100 > ./experiment/nohup32.out 2>&1
+nohup java -Xmx4g -XX:+UseParallelGC -cp ./experiment/benchmarks-main.jar ai.timefold.solver.benchmarks.competitive.cvrplib.Main COMMUNITY_EDITION 400 > ./experiment/nohup33.out 2>&1
+nohup java -Xmx4g -XX:+UseParallelGC -cp ./experiment/benchmarks-main.jar ai.timefold.solver.benchmarks.competitive.cvrplib.Main COMMUNITY_EDITION 400 > ./experiment/nohup34.out 2>&1
+nohup java -Xmx4g -XX:+UseParallelGC -cp ./experiment/benchmarks-main.jar ai.timefold.solver.benchmarks.competitive.cvrplib.Main ENTERPRISE_EDITION 400 > ./experiment/nohup35.out 2>&1
+nohup java -Xmx4g -XX:+UseParallelGC -cp ./experiment/benchmarks-restart.jar ai.timefold.solver.benchmarks.competitive.cvrplib.Main COMMUNITY_EDITION 1000 > ./experiment/nohup36.out 2>&1
 
 sudo shutdown -h now
