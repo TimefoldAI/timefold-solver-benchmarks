1f52422ce0831401dbbf53193e7997c14b616239
Lukáš Petrovický <lukas@petrovicky.net>
2025-04-04 11:27:52 +0200

    isSolutionInitialized() is no more

--- src/main/java/ai/timefold/solver/benchmarks/competitive/Result.java
+++ src/main/java/ai/timefold/solver/benchmarks/competitive/Result.java
@@ -3,7 +3,8 @@ package ai.timefold.solver.benchmarks.competitive;
 import java.time.Duration;
 
 import ai.timefold.solver.core.api.score.Score;
+import ai.timefold.solver.core.impl.score.director.InnerScore;
 
-public record Result<Dataset_ extends Dataset, Score_ extends Score<Score_>>(Dataset_ dataset, Score_ score, int locationCount,
-        int vehicleCount, Duration runtime) {
+public record Result<Dataset_ extends Dataset<Dataset_>, Score_ extends Score<Score_>>(Dataset_ dataset,
+        InnerScore<Score_> score, int locationCount, int vehicleCount, Duration runtime) {
 }
