013e2a776632162660ff9e191346b009765173fe
Christopher Chianelli <christopher@timefold.ai>
2025-05-29 16:19:49 -0400

    feat: Make examination use declarative shadow variables

--- src/main/java/ai/timefold/solver/benchmarks/examples/examination/domain/FollowingExam.java
+++ src/main/java/ai/timefold/solver/benchmarks/examples/examination/domain/FollowingExam.java
@@ -1,8 +1,8 @@
 package ai.timefold.solver.benchmarks.examples.examination.domain;
 
-import ai.timefold.solver.benchmarks.examples.examination.domain.solver.PeriodUpdatingVariableListener;
 import ai.timefold.solver.core.api.domain.entity.PlanningEntity;
 import ai.timefold.solver.core.api.domain.variable.ShadowVariable;
+import ai.timefold.solver.core.preview.api.domain.variable.declarative.ShadowSources;
 
 @PlanningEntity
 public class FollowingExam extends Exam {
@@ -21,15 +21,18 @@ public class FollowingExam extends Exam {
     }
 
     @Override
-    @ShadowVariable(variableListenerClass = PeriodUpdatingVariableListener.class,
-            sourceEntityClass = LeadingExam.class,
-            sourceVariableName = "period")
+    @ShadowVariable(supplierName = "periodSupplier")
     public Period getPeriod() {
         return period;
     }
 
     public void setPeriod(Period period) {
         this.period = period;
+    }
+
+    @ShadowSources("leadingExam.period")
+    public Period periodSupplier() {
+        return leadingExam.getPeriod();
     }
 
     // ************************************************************************
