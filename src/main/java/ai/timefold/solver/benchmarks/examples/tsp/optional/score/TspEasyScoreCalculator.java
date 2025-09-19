cfef158829d7f3d39361eeb0e8145b257be648d3
fred <zepfred@gmail.com>
2025-09-19 08:21:41 -0300

    chore: fix score corruption

--- src/main/java/ai/timefold/solver/benchmarks/examples/tsp/optional/score/TspEasyScoreCalculator.java
+++ src/main/java/ai/timefold/solver/benchmarks/examples/tsp/optional/score/TspEasyScoreCalculator.java
@@ -1,13 +1,6 @@
 package ai.timefold.solver.benchmarks.examples.tsp.optional.score;
 
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
-
-import ai.timefold.solver.benchmarks.examples.tsp.domain.Domicile;
-import ai.timefold.solver.benchmarks.examples.tsp.domain.Standstill;
 import ai.timefold.solver.benchmarks.examples.tsp.domain.TspSolution;
-import ai.timefold.solver.benchmarks.examples.tsp.domain.Visit;
 import ai.timefold.solver.core.api.score.buildin.simplelong.SimpleLongScore;
 import ai.timefold.solver.core.api.score.calculator.EasyScoreCalculator;
 
@@ -15,23 +8,10 @@ public class TspEasyScoreCalculator implements EasyScoreCalculator<TspSolution, 
 
     @Override
     public SimpleLongScore calculateScore(TspSolution tspSolution) {
-        List<Visit> visitList = tspSolution.getVisitList();
-        Set<Visit> tailVisitSet = new HashSet<>(visitList);
-        long score = 0L;
-        for (Visit visit : visitList) {
-            Standstill nextStandsstill = visit.getNextStandstill();
-            if (nextStandsstill != null) {
-                score -= visit.getDistanceToNextStandstill();
-                if (nextStandsstill instanceof Visit) {
-                    tailVisitSet.remove(nextStandsstill);
-                }
-            }
-        }
-        Domicile domicile = tspSolution.getDomicile();
-        for (Visit tailVisit : tailVisitSet) {
-            if (tailVisit.getNextStandstill() == null) {
-                score -= tailVisit.getDistanceTo(domicile);
-            }
+        var visitList = tspSolution.getVisitList();
+        long score = -(tspSolution.getDomicile().getDistanceToNextStandstill());
+        for (var visit : visitList) {
+            score -= visit.getDistanceToNextStandstill();
         }
         return SimpleLongScore.of(score);
     }
