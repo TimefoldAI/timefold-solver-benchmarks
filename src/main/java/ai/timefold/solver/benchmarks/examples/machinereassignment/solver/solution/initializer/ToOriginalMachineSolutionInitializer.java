3c87c384663ddd3c2bf4349da69987d80dce5217
fred <zepfred@gmail.com>
2026-02-23 19:01:51 -0300

    chore: fix custom phases

--- src/main/java/ai/timefold/solver/benchmarks/examples/machinereassignment/solver/solution/initializer/ToOriginalMachineSolutionInitializer.java
+++ src/main/java/ai/timefold/solver/benchmarks/examples/machinereassignment/solver/solution/initializer/ToOriginalMachineSolutionInitializer.java
@@ -1,31 +1,30 @@
 package ai.timefold.solver.benchmarks.examples.machinereassignment.solver.solution.initializer;
 
-import java.util.function.BooleanSupplier;
-
 import ai.timefold.solver.benchmarks.examples.machinereassignment.domain.MachineReassignment;
 import ai.timefold.solver.benchmarks.examples.machinereassignment.domain.MrMachine;
 import ai.timefold.solver.benchmarks.examples.machinereassignment.domain.MrProcessAssignment;
-import ai.timefold.solver.core.api.score.director.ScoreDirector;
 import ai.timefold.solver.core.api.solver.phase.PhaseCommand;
+import ai.timefold.solver.core.api.solver.phase.PhaseCommandContext;
+import ai.timefold.solver.core.preview.api.domain.metamodel.PlanningVariableMetaModel;
+import ai.timefold.solver.core.preview.api.move.builtin.Moves;
 
 public class ToOriginalMachineSolutionInitializer implements PhaseCommand<MachineReassignment> {
 
     @Override
-    public void changeWorkingSolution(ScoreDirector<MachineReassignment> scoreDirector, BooleanSupplier isPhaseTerminated) {
-        MachineReassignment machineReassignment = scoreDirector.getWorkingSolution();
-        initializeProcessAssignmentList(scoreDirector, machineReassignment);
+    public void changeWorkingSolution(PhaseCommandContext<MachineReassignment> phaseCommandContext) {
+        MachineReassignment machineReassignment = phaseCommandContext.getWorkingSolution();
+        initializeProcessAssignmentList(phaseCommandContext, machineReassignment);
     }
 
-    private void initializeProcessAssignmentList(ScoreDirector<MachineReassignment> scoreDirector,
+    private void initializeProcessAssignmentList(PhaseCommandContext<MachineReassignment> phaseCommandContext,
             MachineReassignment machineReassignment) {
+        var variableMetaModel =
+                (PlanningVariableMetaModel<MachineReassignment, MrProcessAssignment, MrMachine>) phaseCommandContext
+                        .getSolutionMetaModel().entity(MrProcessAssignment.class).<MrMachine> variable("machine");
         for (MrProcessAssignment processAssignment : machineReassignment.getProcessAssignmentList()) {
             MrMachine originalMachine = processAssignment.getOriginalMachine();
             MrMachine machine = originalMachine == null ? machineReassignment.getMachineList().get(0) : originalMachine;
-            scoreDirector.beforeVariableChanged(processAssignment, "machine");
-            processAssignment.setMachine(machine);
-            scoreDirector.afterVariableChanged(processAssignment, "machine");
-            scoreDirector.triggerVariableListeners();
+            phaseCommandContext.execute(Moves.change(variableMetaModel, processAssignment, machine));
         }
     }
-
 }
